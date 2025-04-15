4c192f94fcd66e90c39ad96ca2b5d49192fcb47c
Raghavi <raghavip@uw.edu>
2025-04-15 11:11:07 -0700

    Worked on creating a 2d representation for lace ground using a Bthread object to represent each string for torchon ground but adaptable to to other grounds

--- trial1.java
+++ trial1.java
@@ -2,20 +2,28 @@ import java.util.HashMap;
 
 public class trial1 {
   //Map of ground to stitch
-  static HashMap<String,int[]> groundToBraids = new HashMap<>();
+  static HashMap<String,BThread[]> groundToBraids = new HashMap<>();
   static {
     //NOTE: make the array a BThread array
-    groundToBraids.put("torchon", new int[]{2,-1,-3,2,-1,-3});
+    groundToBraids.put("torchon", new BThread[]{
+      new BThread(2),
+      new BThread(-1),
+      new BThread(-3),
+      new BThread(2),
+      new BThread(-1),
+      new BThread(-3)
+  });
+
   }
-  //print and debug 2d array of threads
+  //print to debug 2d array of threads
   public static void main(String[] args) {
     print3DArray(generateGround(5, 5, "torchon"));
   }
 
-  public static int[][][] generateGround(int rows, int cols, String ground) {
+  public static BThread[][][] generateGround(int rows, int cols, String ground) {
     int colsVisual = cols + (cols - 1);
-    int[][][] laceRepresentation = new int[rows][colsVisual][];
-    int[] braidGroup = groundToBraids.get(ground);
+    BThread[][][] laceRepresentation = new BThread[rows][colsVisual][];
+    BThread[] braidGroup = groundToBraids.get(ground);
 
     for (int i = 0; i < rows; i++){
       for (int j = 0; j < colsVisual; j++) {
@@ -24,14 +32,14 @@ public class trial1 {
           if (j % 2 == 1) {
             laceRepresentation[i][j] = braidGroup;
           } else {
-            laceRepresentation[i][j] = new int[0];
+            laceRepresentation[i][j] = new BThread[0];
           }
         }
         else {
           if (j % 2 == 0) {
             laceRepresentation[i][j] = braidGroup;
            } else {
-            laceRepresentation[i][j] = new int[0];
+            laceRepresentation[i][j] = new BThread[0];
           }
         }
       }
@@ -41,7 +49,7 @@ public class trial1 {
   }
 
   //print array
-  public static void print3DArray(int[][][] arr) {
+  public static void print3DArray(BThread[][][] arr) {
     for (int i = 0; i < arr.length; i++) {
         System.out.print("Row " + i + ": ");
         for (int j = 0; j < arr[i].length; j++) {
